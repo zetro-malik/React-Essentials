04f5c77f86b9f1dff3dd67b1194b83826057f8d4
zetro-malik <zeeshanimsb@gmail.com>
2023-09-23 21:05:16 +0500

    add props

---
+++ src/examples/functionalComponents.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Car2(props) {
+    return <h2>Hi, I am {props.dataX}</h2>;
+  }
+
+export default Car2;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Car from './examples/classComponent';
+import Car2 from './examples/functionalComponents';
+
+const name = "zeeshan azhar"
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render();
+root.render(<Car2 dataX = {name}/>);
