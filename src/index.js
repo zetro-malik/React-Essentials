1ae7a5086d43508b5a792a84c8f7663d399aaebb
zetro-malik <zeeshanimsb@gmail.com>
2023-09-23 21:14:22 +0500

    add conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Car from './examples/classComponent';
 import Car2 from './examples/functionalComponents';
+import Goal from './examples/conditional';
 
 const name = "zeeshan azhar"
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Car2 dataX = {name}/>);
+//root.render(<Car2 dataX = {name}/>);
+root.render(<Goal isGoal = {true}/>);
