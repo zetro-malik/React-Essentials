55c66daf89daed5323974214716f6a5953f7b4ee
zetro-malik <zeeshanimsb@gmail.com>
2023-09-23 21:10:47 +0500

    add react click event with args

--- src/examples/functionalComponents.jsx
+++ src/examples/functionalComponents.jsx
@@ -3,14 +3,16 @@ import React from "react";
 
 function Car2(props) {
 
-    const shoot = () => {
-        alert("Great Shot!");
+    const shoot = (arg,evnt) => {
+        // tell what event it was
+        console.log(evnt);
+        alert(`Great Shot! ${arg}`);
       }
 
     return (
         <div>
             <h2>Hi, I am {props.dataX}</h2>
-            <button onClick={shoot}>click me</button>
+            <button onClick={(event)=>shoot("zetro",event)}>click me</button>
         </div>
     );
   }
